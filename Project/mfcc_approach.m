7d77a8e30f24e91657ba133303baf0fe835f8791
Sanad <sanad.shaha.1@gmail.com>
2026-03-29 17:05:13 +0530

    Create mfcc_approach.m

---
+++ Project/mfcc_approach.m
@@ -0,0 +1,82 @@
+%% %% MFCC SPECTROGRAM multiple %% %%
+
+close all
+clc
+clear all
+%% loading data
+ads=audioDatastore('E:\Sanad sem 8\SP4ML\Project\speech_data', 'IncludeSubfolders', true);
+
+%% get speaker ids
+filepaths=ads.Files;
+[~, names, ~]=cellfun(@fileparts, filepaths, 'UniformOutput', false);  % extracts filepath name and extension
+speakerIDs=cellfun(@(x) strtok(x, '_'), names, 'UniformOutput', false); % gives student id, all before _ underscore
+uniqueStudents=unique(speakerIDs);
+
+
+%% create dictionary
+numStudents=100; % can be modified as per requirements
+numFilesperstudent=10;
+featureSize=14; % mfcc gives 13 by default +1 for volume
+D=zeros(featureSize, numStudents);
+
+for i=1:numStudents
+    currentID=uniqueStudents{i}; % for the student in the dictionary
+    studentFiles=filepaths(strcmp(speakerIDs,currentID)); % finds files belonging to a particular student
+    n=min(numFilesperstudent,length(studentFiles));
+    tempmat=zeros(featureSize,n);
+    
+    for j=1:n
+        [audio, fs]=audioread(studentFiles{j});
+        coeffs=mfcc(audio,fs);
+        tempmat(:,j)=mean(coeffs,1)';
+    end
+
+    
+    D(:,i)=mean(tempmat,2)'; % mean coz we have to create a fixed length vector
+end
+
+fprintf('Dictionary D successfully created with size %d x %d\n', size(D,1), size(D,2));
+% every column of D corresponds to a sudent
+
+%% testing for 1000 comparisons
+
+numIterations=1000; 
+correctCount=0;
+
+for i=1:numIterations
+    trueidx=randi(numStudents);
+    testID=uniqueStudents{trueidx};
+    testFiles=filepaths(strcmp(speakerIDs, testID));
+
+    if length(testFiles)>8  % not used in training D
+        fileIdx=randi([8,length(testFiles)]);
+    else
+        % If a student only has 1 file total, we skip this iteration or use the same file (but accuracy will be artificially high)
+        fileIdx = length(testFiles); 
+    end
+
+    % extract features
+    [testAudio, testFs]=audioread(testFiles{fileIdx});
+    testCoeffs=mfcc(testAudio,testFs);
+    y=mean(testCoeffs,1)';
+
+    dists=zeros(1,numStudents);
+    for k=1:numStudents
+        dists(k)=norm(y-D(:,k),2);
+    end
+
+    [~,recognizedidx]=min(dists);
+    if recognizedidx==trueidx
+        correctCount=correctCount+1;
+    end
+
+end
+
+% --- Final Stats ---
+totalAccuracy = (correctCount / numIterations) * 100;
+fprintf('==============================\n')
+fprintf('Dictionary Size: %d Students\n', numStudents);
+fprintf('Trials Run: %d \n', numIterations);
+fprintf('Correct Matches: %d\n', correctCount);
+fprintf('Estimated Accuracy: %.2f%%\n', totalAccuracy);
+fprintf('==============================\n')
